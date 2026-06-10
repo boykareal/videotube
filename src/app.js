15ababf10c5ecf99513122fb5dd382534d94a299
boykareal <vaibhavx3sharma@gmail.com>
2026-06-10 23:12:37 +0530

    add health check router middleware to route

--- src/app.js
+++ src/app.js
@@ -14,6 +14,11 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({ extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 
+import healthcheckRouter from'./routes/healthcheck.routes.js'
+
+
+
+app.use(`api/v1/healthcheck`,healthcheckRouter)
 
 
 
