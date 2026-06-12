fa7192a8f92ff8d3fd7612ce93faa4671a7bacd7
boykareal <vaibhavx3sharma@gmail.com>
2026-06-12 21:57:15 +0530

    add method in schema for ecrypting password

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose";
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
     {
@@ -48,5 +49,20 @@ const userSchema = new Schema(
         timestamps: true
     }
 )
+
+userSchema.pre("save", async function (next){
+
+    if(!this.modified("password")) return next()
+    this.password = bcrypt.hash(this.password, 10)
+
+
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.comapare(password, this.password)
+}
+
+
 
 export const User = mongoose.model("User",userSchema)
