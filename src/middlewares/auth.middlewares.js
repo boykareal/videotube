5c32833fa81618dab2ddf86d99925136dbeca234
boykareal <vaibhavx3sharma@gmail.com>
2026-06-19 05:09:13 +0530

    add verify jwt middleware

---
+++ src/middlewares/auth.middlewares.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken"
+import { ApiError } from "../utils/api-error.js"
+import { User } from "../models/user.models.js"
+import { asynchandler } from "../utils/async-handler.js"
+
+
+export const verifyJWT = asynchandler(async(req, _, next)=> {
+    const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+    if(!token){
+        throw new ApiError(401, "Unauthorized")
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+        if(!user){
+            throw new ApiError(401,"Unauthorized")
+        }
+
+        req.user = user
+
+        next()
+
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+}) 
