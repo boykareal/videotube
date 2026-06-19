7e9efbab91eadfea9d46cccb9a8c523e169c5071
boykareal <vaibhavx3sharma@gmail.com>
2026-06-19 23:16:37 +0530

    add get current user functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -235,6 +235,7 @@ const changeCurrentPassword = asynchandler(async (req, res)=> {
 })
 
 const getCurrentUser = asynchandler(async (req, res)=> {
+    return res.status(200).json(new ApiResponse(200, req.user, "Current user details"))
 })
 
 const updateAccountDetails = asynchandler(async (req, res)=> {
