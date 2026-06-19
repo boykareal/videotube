336d710d07a8de8d211a8113d987fa454cbe7c54
boykareal <vaibhavx3sharma@gmail.com>
2026-06-20 00:33:08 +0530

    add get user channel profile functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -312,6 +312,84 @@ const updateUserCoverImage = asynchandler(async (req, res)=> {
     return res.status(200).json(new ApiResponse(200, user, "Cover image updated successfully"))
 })
 
+const getUserChannelProfile = asynchandler( async(req, res)=> {
+    const {username} = req.params
+
+    if(!username?.trim()){
+        throw new ApiError(400, "Username is required")
+    }
+
+    const channel = await User.aggregate(
+        [
+            {
+                $match: {
+                    username: username?.toLowerCase()
+                }
+            },
+            {
+                $lookup: {
+                    from: "subscription",
+                    localField: "_id",
+                    foreignField: "channel",
+                    as: "subscribers"
+                }
+            },
+            {
+                $lookup: {
+                    from: "subscription",
+                    localField: "_id",
+                    foreignField: "subscriber",
+                    as: "subscribedto"
+                }
+            },
+            {
+                $addFields:{
+                    subscribersCount: {
+                        $size: "$subscribers"
+                    },
+                    channelSubscribedToCount: {
+                        $size: "$subscribedto"
+                    },
+                    isSubscribed: {
+                        $cond: {
+                            if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                            then: true,
+                            else: false
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                    subscribersCount: 1,
+                    channelSubscribedToCount: 1,
+                    isSubscribed: 1,
+                    coverImage: 1
+                }
+            }
+        ]
+    )
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel not found")
+    }
+
+    res.status(200)
+    .json(new ApiResponse(
+        200,
+        channel[0],
+        "Channel profile fetched successfully"
+    ))
+
+})
+
+const getWatchHistory = asynchandler( async(req, res)=> {
+    
+})
+
 
 
 
